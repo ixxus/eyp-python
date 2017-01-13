e021d1a945c852853726f3e3e27f05bbe84d7b9d
Jordi Prats <jprats@systemadmin.es>
2017-01-13 14:36:32 +0100

    ubuntu14.04 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,18 +1,39 @@
 class python::params {
 
+	$python_pkgs= [ 'python', 'python-pip' ]
+
 	case $::osfamily
 	{
 		'redhat':
 		{
+			$include_epel=true
 			case $::operatingsystemrelease
 			{
 				/^6.*$/:
 				{
-          $python_pkgs= [ 'python', 'python-pip' ]
 				}
-				default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
+				default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
 			}
 		}
-		default: { fail("Unsupported OS!")  }
+		'Debian':
+		{
+			$include_epel=false
+			case $::operatingsystem
+			{
+				'Ubuntu':
+				{
+					case $::operatingsystemrelease
+					{
+						/^14.*$/:
+						{
+						}
+						default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
+					}
+				}
+				'Debian': { fail('Unsupported')  }
+				default: { fail('Unsupported Debian flavour!')  }
+			}
+		}
+		default: { fail('Unsupported OS!')  }
 	}
 }
